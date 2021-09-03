29258c672ed693573e9f5edb043605f127890b27
Omar-Ess <souafomar@gmail.com>
2021-09-03 15:15:37 +0100

    fixed some image issues and responsiveness in clients section

--- components/home/Clients.js
+++ components/home/Clients.js
@@ -4,6 +4,25 @@ import Heading from "../base/Heading";
 
 const Clients = () => {
   const { t } = useTranslation("common");
+  const clients = [
+    {
+      image: "tousalik.png",
+    },
+    {
+      image: "volzak.png",
+    },
+    {
+      image: "yankees.jpg",
+      bgColor: "bg-gray-900",
+    },
+    {
+      image: "vcc-club.png",
+    },
+    {
+      image: "xeryos.png",
+      bgColor: "bg-blue-800",
+    },
+  ];
   return (
     <div className="my-10">
       <div className="flex flex-col items-center space-y-5 mb-10 px-5 mt-10">
@@ -12,41 +31,29 @@ const Clients = () => {
           {t("ourClients")}
         </Heading>
       </div>
-      <div className="container py-5 flex items-center justify-between">
-        <Image
-          src="/images/clients/tousalik.png"
-          className="opacity-80 hover:opacity-100 transition-all duration-150"
-          width={140}
-          height={100}
-        />
-        <Image
-          src="/images/clients/volzak.png"
-          width={140}
-          height={100}
-          className="opacity-80 hover:opacity-100 transition-all duration-150"
-        />
-        <div className="bg-gray-900 py-2 px-3 rounded-md">
-          <Image
-            src="/images/clients/yankees.jpg"
-            width={190}
-            height={80}
-            className="opacity-80 hover:opacity-100 transition-all duration-150"
-          />
-        </div>
-        <Image
-          src="/images/clients/vcc-club.png"
-          width={140}
-          height={100}
-          className="opacity-80 hover:opacity-100 transition-all duration-150"
-        />
-        <div className="bg-blue-800 py-2 px-3 rounded-md">
-          <Image
-            src="/images/clients/xeryos.png"
-            width={190}
-            height={80}
-            className="opacity-80 hover:opacity-100 transition-all duration-150"
-          />
-        </div>
+      <div className="container py-5 flex gap-4 items-center lg:justify-between">
+        {clients.map((client, index) => {
+          return client.bgColor ? (
+            <div key={index} className={`${client.bgColor} py-2 px-3 rounded-md`}>
+              <Image
+                src={`/images/clients/${client.image}`}
+                width={190}
+                height={80}
+                alt={client.image}
+                className="opacity-80 hover:opacity-100 transition-all duration-150"
+              />
+            </div>
+          ) : (
+            <Image
+              key={index}
+              src={`/images/clients/${client.image}`}
+              className="opacity-80 hover:opacity-100 transition-all duration-150"
+              width={140}
+              height={100}
+              alt={client.image}
+            />
+          );
+        })}
       </div>
     </div>
   );
