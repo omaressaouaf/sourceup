2253643e164d2b8bbcceff3762167ce8230dead8
Omar-Ess <souafomar@gmail.com>
2021-10-13 13:54:36 +0100

    included the word source up in the hero for seo

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -17,14 +17,12 @@ const Hero = () => {
               <p className="px-2 text-gray-400">{t("discountText")}</p>
             </div>
             <h1 className="mb-8 text-2xl font-extrabold tracking-tight lg:text-5xl">
-              {t("heroTitle")}
+              {"Source Up " + t("heroTitle")}
               <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-brand via-brand to-brand-light">
                 {t("webAndMobile")}
               </span>
             </h1>
-            <p className="mb-8 text-base leading-relaxed text-left text-gray-400">
-              {t("heroDescription")}
-            </p>
+            <p className="mb-8 text-base leading-relaxed text-left text-gray-400">{t("heroDescription")}</p>
             <div className="flex flex-col justify-center lg:flex-row">
               <Link href="/services">
                 <a>
@@ -37,13 +35,7 @@ const Hero = () => {
 
         <div className="w-50 relative flex-grow">
           <ScrollAnimation animation="zoom-in">
-            <Image
-              alt="hero"
-              width="1750"
-              height="1000"
-              layout="intrinsic"
-              src="/images/hero.svg"
-            />
+            <Image alt="hero" width="1750" height="1000" layout="intrinsic" src="/images/hero.svg" />
           </ScrollAnimation>
         </div>
       </div>
